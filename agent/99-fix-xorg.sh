41efe09ff1fe678293629e841d25008ca7f2a36c
titoman81 <wevas36@gmail.com>
2026-07-05 02:58:32 -0400

    Use COPY for 99-fix-xorg.sh to fix awk syntax escaping

---
+++ agent/99-fix-xorg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -f /etc/X11/xorg.conf ]; then
+    if ! grep -q "Virtual 1920 1080" /etc/X11/xorg.conf; then
+        echo "Inyectando resolución virtual en xorg.conf..."
+        awk '/Section "Screen"/{print;print "    SubSection \"Display\"\n        Depth 24\n        Virtual 1920 1080\n    EndSubSection";next}1' /etc/X11/xorg.conf > /tmp/xorg.conf && mv /tmp/xorg.conf /etc/X11/xorg.conf
+    fi
+fi
+echo "capture = x11" >> /templates/sunshine/sunshine.conf
