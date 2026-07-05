5743293fccc05050903ac2e14e8c9c9c589f07ee
titoman81 <wevas36@gmail.com>
2026-07-05 03:09:40 -0400

    Inject UseDisplayDevice none to bypass DRM modesetting

--- agent/99-fix-xorg.sh
+++ agent/99-fix-xorg.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ -f /etc/X11/xorg.conf ]; then
     if ! grep -q "Virtual 1920 1080" /etc/X11/xorg.conf; then
-        echo "Inyectando resolución virtual en xorg.conf..."
+        echo "Inyectando resolución virtual y flag headless en xorg.conf..."
         awk '/Section "Screen"/{print;print "    SubSection \"Display\"\n        Depth 24\n        Virtual 1920 1080\n    EndSubSection";next}1' /etc/X11/xorg.conf > /tmp/xorg.conf && mv /tmp/xorg.conf /etc/X11/xorg.conf
+        sed -i '/Driver         "nvidia"/a \    Option         "UseDisplayDevice" "none"' /etc/X11/xorg.conf
     fi
 fi
 echo "capture = x11" >> /templates/sunshine/sunshine.conf
